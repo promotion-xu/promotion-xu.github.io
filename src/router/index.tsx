fb3db38f9d863020d334940cdb30da0c1f1262b0
promotion-xu <17521337708@163.com>
2022-10-12 20:39:59 +0800

    fix: relative path

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "@/pages/home";
+// import Home from "@/pages/home";
+import Home from '../pages/home'
 import JsPractice from "@/pages/js-practice";
 import GithubAction from "@/pages/github/action";
 
