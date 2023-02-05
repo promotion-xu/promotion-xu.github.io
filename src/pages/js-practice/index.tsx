4953eb95d25aaf17fa7c602ca9e91a491f89eda3
promotion-xu <17521337708@163.com>
2023-02-05 19:30:17 +0800

    cr/feature-6

--- src/pages/js-practice/index.tsx
+++ src/pages/js-practice/index.tsx
@@ -12,20 +12,38 @@ const JsPractice = () => {
 
 
   return <div>
-     <Tabs activeKey={activeKey}>
-      <Tabs.TabPane tab="Tab 1" key="1">
-        <div>Content of Tab Pane 1</div>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Tab 2" key="2">
-        <div>Content of Tab Pane 2</div>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Tab 3" key="3">
-        <div>Content of Tab Pane 3</div>
-      </Tabs.TabPane>
-      <Tabs.TabPane tab="Tab 4" key="4">
-        <div>Content of Tab Pane 4</div>
-      </Tabs.TabPane>
-    </Tabs>
+     {
+      activeKey === '1' ? <Tabs activeKey={activeKey}>
+        <Tabs.TabPane tab="Tab 1" key="1">
+          <div>Content of Tab Pane 1</div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Tab 2" key="2">
+          <div>Content of Tab Pane 2</div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Tab 3" key="3">
+          <div>Content of Tab Pane 3</div>
+        </Tabs.TabPane>
+        <Tabs.TabPane tab="Tab 4" key="4">
+          <div>Content of Tab Pane 4</div>
+        </Tabs.TabPane>
+      </Tabs> : 
+      <div>
+        <Tabs activeKey={activeKey}>
+          <Tabs.TabPane tab="Tab 1" key="1">
+            <div>Content of Tab Pane 1</div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Tab 2" key="2">
+            <div>Content of Tab Pane 2</div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Tab 3" key="3">
+            <div>Content of Tab Pane 3</div>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Tab 4" key="4">
+            <div>Content of Tab Pane 4</div>
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
+    }
   </div>
 }
 
