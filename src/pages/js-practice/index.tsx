0c7573da8cca0dee5e33f647f2a258120f7c3227
promotion-xu <17521337708@163.com>
2023-02-11 21:25:11 +0800

    cr/feature-6

--- src/pages/js-practice/index.tsx
+++ src/pages/js-practice/index.tsx
@@ -1,50 +1,50 @@
-import React, { useState } from 'react'
-import { Tabs } from 'antd';
+import React, { useState } from "react"
+import { Tabs } from "antd"
 
 const JsPractice = () => {
 
 
-  const [activeKey, setActiveKey] = useState('1')
+	const [activeKey, setActiveKey] = useState("1")
 
-  const fetchList = async () => {
-    const res = await getDataList()
-  }
+	const fetchList = async () => {
+		const res = await getDataList()
+	}
 
 
-  return <div>
-     {
-      activeKey === '1' ? <Tabs activeKey={activeKey}>
-        <Tabs.TabPane tab="Tab 1" key="1">
-          <div>Content of Tab Pane 1</div>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Tab 2" key="2">
-          <div>Content of Tab Pane 2</div>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Tab 3" key="3">
-          <div>Content of Tab Pane 3</div>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Tab 4" key="4">
-          <div>Content of Tab Pane 4</div>
-        </Tabs.TabPane>
-      </Tabs> : 
-      <div>
-        <Tabs activeKey={activeKey}>
-          <Tabs.TabPane tab="Tab 1" key="1">
-            <div>Content of Tab Pane 1</div>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Tab 2" key="2">
-            <div>Content of Tab Pane 2</div>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Tab 3" key="3">
-            <div>Content of Tab Pane 3</div>
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="Tab 4" key="4">
-            <div>Content of Tab Pane 4</div>
-          </Tabs.TabPane>
-        </Tabs>
-      </div>
-    }
-  </div>
+	return <div>
+		{
+			activeKey === "1" ? <Tabs activeKey={activeKey}>
+				<Tabs.TabPane tab="Tab 1" key="1">
+					<div>Content of Tab Pane 1</div>
+				</Tabs.TabPane>
+				<Tabs.TabPane tab="Tab 2" key="2">
+					<div>Content of Tab Pane 2</div>
+				</Tabs.TabPane>
+				<Tabs.TabPane tab="Tab 3" key="3">
+					<div>Content of Tab Pane 3</div>
+				</Tabs.TabPane>
+				<Tabs.TabPane tab="Tab 4" key="4">
+					<div>Content of Tab Pane 4</div>
+				</Tabs.TabPane>
+			</Tabs> : 
+				<div>
+					<Tabs activeKey={activeKey}>
+						<Tabs.TabPane tab="Tab 1" key="1">
+							<div>Content of Tab Pane 1</div>
+						</Tabs.TabPane>
+						<Tabs.TabPane tab="Tab 2" key="2">
+							<div>Content of Tab Pane 2</div>
+						</Tabs.TabPane>
+						<Tabs.TabPane tab="Tab 3" key="3">
+							<div>Content of Tab Pane 3</div>
+						</Tabs.TabPane>
+						<Tabs.TabPane tab="Tab 4" key="4">
+							<div>Content of Tab Pane 4</div>
+						</Tabs.TabPane>
+					</Tabs>
+				</div>
+		}
+	</div>
 }
 
 export default JsPractice
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,30 +1,12 @@
 
 
-export const sum = (a: number, b: number) => {
-  return a + b
-}
-
-
-export const statusMap = (status: string) => {
-  switch(status) {
-    case 'success':
-      return 'Successfully'
-    case 'fail':
-      return 'failed'
-    case 'warn':
-      return 'warning'
-    case 'danger':
-      return 'dangerous'
-    case 'info':
-      return 'information'
-    case 'text':
-      return 'texts'
-    default:
-      return status
-  }
-}
-
-
-export const fetchList = () => {
-  return fetch(`/api/user/list/messages`)
+export const statusMap = {
+	init: {
+		id: "start",
+		status: "success"
+	},
+	processing: {
+		id: "process",
+		status: "warning"
+	}
 }
