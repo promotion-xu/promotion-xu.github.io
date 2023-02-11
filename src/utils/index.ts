da915c014e2cd0715061c035d55ae5058f69191a
promotion-xu <17521337708@163.com>
2023-02-11 22:02:08 +0800

    cr/feature-7

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,10 +3,22 @@
 export const statusMap = {
 	init: {
 		id: "start",
-		status: "success"
+		status: "default"
 	},
 	processing: {
 		id: "process",
 		status: "warning"
+	},
+	completed: {
+		id: "completed",
+		status: "success"
 	}
+}
+
+type IStatus = "init" | "processing" | "completed"
+
+export interface IInfo {
+  name: string;
+  age: string;
+  status: IStatus
 }
